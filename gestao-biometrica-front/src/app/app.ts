705b2a2436e484a3e4100fd6bcb4fddfd740643c
Paulo Peso <paulojosuevictorpeso23@gmail.com>
2026-06-03 00:43:14 +0100

    commit 2

--- gestao-biometrica-front/src/app/app.ts
+++ gestao-biometrica-front/src/app/app.ts
@@ -5,7 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Hello, {{ title() }}</h1>
+    <h1 style="color: #0000; font-family: sans-serif;">
+      Sistema de Gestão Biométrica - UAN
+    </h1>
+    <p style="font-family: sans-serif;">O nosso Frontend Angular já está a funcionar!</p>
 
     <router-outlet />
   `,
